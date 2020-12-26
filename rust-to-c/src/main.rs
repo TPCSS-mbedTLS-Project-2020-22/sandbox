f84c0d4b9cb70ecd1af06732672839bdde983f90
Alvin <alvingeorge>
2020-12-26 22:17:12 +0530

    calling rust->c  through a wrapper func, and returning an i32

--- rust-to-c/src/main.rs
+++ rust-to-c/src/main.rs
@@ -17,13 +17,21 @@ extern {
 fn call_c_func() {
 
     let x = 1234;
-    let _y = unsafe {  func_in_c(x);  };      // '_'  here says y wont be used.  avoids the warning
+    let _y = unsafe {  func_in_c(x)  };      // '_'  here says y wont be used.  avoids the warning
 }
 
+/************* another style would be to define a wrapper function for each of the external c function ********/
+// it might be a good idea to have a wrapper for each of the external c functions
+fn wrapper_func_in_c(x: i32) -> i32 {
+    let y = unsafe {  func_in_c(x)  };
+    return y;
+}
 
 
 fn main() {
     call_c_func();
 
+    let z = wrapper_func_in_c(1000);
+    println!("calling C through the wrapper func:  {0}", z);
 }
 
